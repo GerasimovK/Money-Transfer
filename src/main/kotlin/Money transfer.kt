162b79a2048b21a37135b5ab21ced51b846df91e
GerasimovK <k.gerasimov@cifrium.ru>
2022-07-21 13:59:42 +0300

    Lesson 2. Task 1

---
+++ src/main/kotlin/Money transfer.kt
@@ -0,0 +1,13 @@
+package ru.cifrium
+
+fun main () {
+    val itemAmount: Int = 20_000
+    val itemCommission: Double = 0.075
+    val CommStart: Int = 3_500
+
+    var totalCommission: Double = itemAmount * itemCommission
+    if (totalCommission <= 3_500) {
+        totalCommission = CommStart.toDouble()
+    }
+    println ("Commission: $totalCommission ")
+}
